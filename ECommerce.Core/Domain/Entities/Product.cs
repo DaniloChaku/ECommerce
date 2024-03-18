d42ddc904b95801f2001ba92fe4688b8ea3fa3ab
Danilo Chaku <danilochaku@gmail.com>
2024-03-18 10:30:17 +0200

    feat: add Product entity

---
+++ ECommerce.Core/Domain/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(400)]
+        public string? Description { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        [Range(0, double.MaxValue)]
+        public decimal? SalePrice { get; set; }
+        public string? ImageUrl { get; set; }
+        [Required]
+        public long Stock { get; set; }
+        public Guid ManufacturerId { get; set; }
+        [ForeignKey(nameof(ManufacturerId))]
+        public Manufacturer Manufacturer { get; set; }
+        public Guid CategoryId { get; set; }
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+    }
+}
