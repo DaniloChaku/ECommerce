63a4ec14d74a12765b61f78d5124389c4b27fe1c
Danilo Chaku <danilochaku@gmail.com>
2024-03-19 16:50:48 +0200

    feat: add Repository<TEntity>

---
+++ ECommerce.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,56 @@
+﻿using ECommerce.Core.Domain.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext _context;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<bool> AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> UpdateAsync(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> DeleteAsync(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            return await SaveAsync();
+        }
+
+        public async Task<bool> SaveAsync()
+        {
+            var saved = await _context.SaveChangesAsync();
+            return saved > 0;
+        }
+    }
+}
