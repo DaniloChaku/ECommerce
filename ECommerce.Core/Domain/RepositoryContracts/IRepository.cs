1e90f538c31ee06db00b9dd0795d6e1aa235a2ff
Danilo Chaku <danilochaku@gmail.com>
2024-03-19 16:23:28 +0200

    feat: add IRepository<TEntity> interface

---
+++ ECommerce.Core/Domain/RepositoryContracts/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Core.Domain.RepositoryContracts
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(Guid id);
+        Task<bool> AddAsync(TEntity entity);
+        Task<bool> UpdateAsync(TEntity entity);
+        Task<bool> DeleteAsync(TEntity entity);
+        Task<bool> SaveAsync();
+    }
+}
