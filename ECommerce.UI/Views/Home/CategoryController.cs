d07239e2fc1104d2f43330c5f17eb2015de8885d
Danilo Chaku <danilochaku@gmail.com>
2024-04-03 18:17:31 +0300

    feat: implement Category controller and views

--- ECommerce.UI/Views/Home/CategoryController.cs
+++ ECommerce.UI/Views/Home/CategoryController.cs
@@ -7,10 +7,18 @@ namespace ECommerce.UI.Views.Home
     public class CategoryController : Controller
     {
         private readonly ICategoryGetterService _categoryGetterService;
+        private readonly ICategoryAdderService _categoryAdderService;
+        private readonly ICategoryUpdaterService _categoryUpdaterService;
+        private readonly ICategoryDeleterService _categoryDeleterService;
 
-        public CategoryController(ICategoryGetterService categoryGetterService)
+        public CategoryController(ICategoryGetterService categoryGetterService, 
+            ICategoryAdderService categoryAdderService, ICategoryUpdaterService categoryUpdaterService,
+            ICategoryDeleterService categoryDeleterService)
         {
             _categoryGetterService = categoryGetterService;
+            _categoryAdderService = categoryAdderService;
+            _categoryUpdaterService = categoryUpdaterService;
+            _categoryDeleterService = categoryDeleterService;
         }
 
         public IActionResult Index()
@@ -19,10 +27,79 @@ namespace ECommerce.UI.Views.Home
         }
 
         [HttpGet]
+        public async Task<IActionResult> Upsert(Guid? id)
+        {
+            if (id is null || id == Guid.Empty) 
+            {
+                return View(new CategoryDto());
+            }
+
+            var existingCategory = await _categoryGetterService.GetByIdAsync(id.Value);
+            if (existingCategory is null)
+            {
+                existingCategory = new CategoryDto();
+            }
+
+            return View(existingCategory);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upsert(CategoryDto categoryDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoryDto);
+            }
+
+            try
+            {
+                if (categoryDto.Id == Guid.Empty)
+                {
+                    await _categoryAdderService.AddAsync(categoryDto);
+                }
+                else
+                {
+                    await _categoryUpdaterService.UpdateAsync(categoryDto);
+                }
+
+                TempData["success"] = $"Category {(categoryDto.Id == Guid.Empty 
+                    ? "created" : "updated")} successfully.";
+                return RedirectToAction(nameof(CategoryController.Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(nameof(categoryDto), ex.Message);
+                TempData["error"] = "An error occurred while processing your request. Please try again.";
+                return View(categoryDto);
+            }
+        }
+
+        #region API
+
+        [HttpGet]
         public async Task<IActionResult> GetAll()
         {
             List<CategoryDto> categoryDtos = await _categoryGetterService.GetAllAsync();
             return Json(new { data = categoryDtos });
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _categoryDeleterService.DeleteAsync(id);
+                TempData["success"] = "Category deleted successfully.";
+                return Json(new { success = true, message = "Deleted Successfully" });
+            }
+            catch(Exception ex)
+            {
+                TempData["error"] = "An error occurred while deleting Category. Please try again.";
+                ModelState.AddModelError(nameof(id), ex.Message);
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+        }
+
+        #endregion
     }
 }
