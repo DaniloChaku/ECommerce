42c34f02ca01ba30a3def9ceaf56ced910b6c2f2
Danilo Chaku <danilochaku@gmail.com>
2024-04-20 19:08:07 +0300

    feat: separate service configurations with ConfigureServices extension

--- ECommerce.UI/Program.cs
+++ ECommerce.UI/Program.cs
@@ -1,53 +1,9 @@
-using ECommerce.Core.Domain.RepositoryContracts;
-using ECommerce.Core.ServiceContracts.Category;
-using ECommerce.Core.ServiceContracts.Image;
-using ECommerce.Core.ServiceContracts.Manufacturer;
-using ECommerce.Core.ServiceContracts.Product;
-using ECommerce.Core.Services.Category;
-using ECommerce.Core.Services.Image;
-using ECommerce.Core.Services.Manufacturer;
-using ECommerce.Core.Services.Product;
-using ECommerce.Core.Settings;
-using ECommerce.Infrastructure.Db;
-using ECommerce.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using ECommerce.UI.StartupExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.Configure<ImageUploadOptions>(builder.Configuration.GetSection("ImageUpload"));
-
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<ICategoryGetterService, CategoryGetterService>();
-builder.Services.AddScoped<ICategoryAdderService, CategoryAdderService>();
-builder.Services.AddScoped<ICategoryUpdaterService, CategoryUpdaterService>();
-builder.Services.AddScoped<ICategoryDeleterService, CategoryDeleterService>();
-builder.Services.AddScoped<ICategorySorterService, CategorySorterService>();
-
-builder.Services.AddScoped<IManufacturerRepository, ManufacturerRepository>();
-builder.Services.AddScoped<IManufacturerGetterService, ManufacturerGetterService>();
-builder.Services.AddScoped<IManufacturerAdderService, ManufacturerAdderService>();
-builder.Services.AddScoped<IManufacturerUpdaterService, ManufacturerUpdaterService>();
-builder.Services.AddScoped<IManufacturerDeleterService, ManufacturerDeleterService>();
-builder.Services.AddScoped<IManufacturerSorterService, ManufacturerSorterService>();
-
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped<IProductGetterService, ProductGetterService>();
-builder.Services.AddScoped<IProductAdderService, ProductAdderService>();
-builder.Services.AddScoped<IProductUpdaterService, ProductUpdaterService>();
-builder.Services.AddScoped<IProductDeleterService, ProductDeleterService>();
-builder.Services.AddScoped<IProductSorterService, ProductSorterService>();
-
-builder.Services.AddScoped<IImageUploaderService, ImageUploaderService>();
-builder.Services.AddScoped<IImageDeleterService, ImageDeleterService>();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
-});
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ ECommerce.UI/StartupExtensions/ConfigureServicesExtensions.cs
@@ -0,0 +1,58 @@
+﻿using ECommerce.Core.Domain.RepositoryContracts;
+using ECommerce.Core.ServiceContracts.Category;
+using ECommerce.Core.ServiceContracts.Image;
+using ECommerce.Core.ServiceContracts.Manufacturer;
+using ECommerce.Core.ServiceContracts.Product;
+using ECommerce.Core.Services.Category;
+using ECommerce.Core.Services.Image;
+using ECommerce.Core.Services.Manufacturer;
+using ECommerce.Core.Services.Product;
+using ECommerce.Core.Settings;
+using ECommerce.Infrastructure.Db;
+using ECommerce.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.UI.StartupExtensions
+{
+    public static class ConfigureServicesExtensions
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddControllersWithViews();
+
+            services.Configure<ImageUploadOptions>(configuration.GetSection("ImageUpload"));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryGetterService, CategoryGetterService>();
+            services.AddScoped<ICategoryAdderService, CategoryAdderService>();
+            services.AddScoped<ICategoryUpdaterService, CategoryUpdaterService>();
+            services.AddScoped<ICategoryDeleterService, CategoryDeleterService>();
+            services.AddScoped<ICategorySorterService, CategorySorterService>();
+
+            services.AddScoped<IManufacturerRepository, ManufacturerRepository>();
+            services.AddScoped<IManufacturerGetterService, ManufacturerGetterService>();
+            services.AddScoped<IManufacturerAdderService, ManufacturerAdderService>();
+            services.AddScoped<IManufacturerUpdaterService, ManufacturerUpdaterService>();
+            services.AddScoped<IManufacturerDeleterService, ManufacturerDeleterService>();
+            services.AddScoped<IManufacturerSorterService, ManufacturerSorterService>();
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductGetterService, ProductGetterService>();
+            services.AddScoped<IProductAdderService, ProductAdderService>();
+            services.AddScoped<IProductUpdaterService, ProductUpdaterService>();
+            services.AddScoped<IProductDeleterService, ProductDeleterService>();
+            services.AddScoped<IProductSorterService, ProductSorterService>();
+
+            services.AddScoped<IImageUploaderService, ImageUploaderService>();
+            services.AddScoped<IImageDeleterService, ImageDeleterService>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("Default"));
+            });
+
+            return services;
+        }
+    }
+}
