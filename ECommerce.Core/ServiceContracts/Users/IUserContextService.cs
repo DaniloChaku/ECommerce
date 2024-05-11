97458c0ccae169f72e984e7d229724ab861d8e01
Danilo Chaku <danilochaku@gmail.com>
2024-05-11 15:26:46 +0300

    docs: document IUserContextService

--- ECommerce.Core/ServiceContracts/Users/IUserContextService.cs
+++ ECommerce.Core/ServiceContracts/Users/IUserContextService.cs
@@ -2,8 +2,18 @@
 
 namespace ECommerce.Core.ServiceContracts.Users
 {
+    /// <summary>
+    /// Defines the contract for accessing user context information.
+    /// </summary>
     public interface IUserContextService
     {
+        /// <summary>
+        /// Retrieves the customer identifier from the provided claims identity.
+        /// </summary>
+        /// <param name="claimsIdentity">The claims identity of the user.</param>
+        /// <returns>
+        /// The customer identifier if found in the claims identity; otherwise, null.
+        /// </returns>
         Guid? GetCustomerId(ClaimsIdentity? claimsIdentity);
     }
 }
