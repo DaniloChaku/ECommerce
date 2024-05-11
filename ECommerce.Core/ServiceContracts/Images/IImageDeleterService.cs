97a1e1eb0bd51b5d4f454a0025670b197b5541dc
Danilo Chaku <danilochaku@gmail.com>
2024-05-11 15:08:09 +0300

    docs: document the image services: Uploader, Deleter

--- ECommerce.Core/ServiceContracts/Images/IImageDeleterService.cs
+++ ECommerce.Core/ServiceContracts/Images/IImageDeleterService.cs
@@ -1,8 +1,20 @@
 ﻿namespace ECommerce.Core.ServiceContracts.Images
 {
+    /// <summary>
+    /// Defines the contract for deleting images.
+    /// </summary>
     public interface IImageDeleterService
     {
+        /// <summary>
+        /// Deletes the image specified by the URL.
+        /// </summary>
+        /// <param name="imageUrl">The URL of the image to delete.</param>
         void DeleteImage(string? imageUrl);
+
+        /// <summary>
+        /// Deletes the image folder associated with the specified product.
+        /// </summary>
+        /// <param name="productId">The identifier of the product whose image folder should be deleted.</param>
         void DeleteImageFolder(string productId);
     }
 }
--- ECommerce.Core/ServiceContracts/Images/IImageUploaderService.cs
+++ ECommerce.Core/ServiceContracts/Images/IImageUploaderService.cs
@@ -2,8 +2,18 @@
 
 namespace ECommerce.Core.ServiceContracts.Images
 {
+    /// <summary>
+    /// Defines the contract for uploading images.
+    /// </summary>
     public interface IImageUploaderService
     {
+        /// <summary>
+        /// Uploads an image asynchronously.
+        /// </summary>
+        /// <param name="image">The image file to upload.</param>
+        /// <param name="productId">The identifier of the product associated with the image.</param>
+        /// <returns>A task that represents the asynchronous operation. 
+        /// The task result contains the URL of the uploaded image.</returns>
         Task<string> UploadAsync(IFormFile image, string productId);
     }
 }
