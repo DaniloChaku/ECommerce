5327e09860c5fe696c08890d6492049b5bca6e4e
Danilo Chaku <danilochaku@gmail.com>
2024-05-11 10:14:29 +0300

    docs: document the identity entities: ApplicationUser, ApplicationRole

--- ECommerce.Core/Domain/IdentityEntities/ApplicationRole.cs
+++ ECommerce.Core/Domain/IdentityEntities/ApplicationRole.cs
@@ -2,6 +2,9 @@
 
 namespace ECommerce.Core.Domain.IdentityEntities
 {
+    /// <summary>
+    /// Represents an application role in the system.
+    /// </summary>
     public class ApplicationRole : IdentityRole<Guid>
     {
     }
--- ECommerce.Core/Domain/IdentityEntities/ApplicationUser.cs
+++ ECommerce.Core/Domain/IdentityEntities/ApplicationUser.cs
@@ -2,9 +2,19 @@
 
 namespace ECommerce.Core.Domain.IdentityEntities
 {
+    /// <summary>
+    /// Represents an application user in the system.
+    /// </summary>
     public class ApplicationUser : IdentityUser<Guid>
     {
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
         public string Name { get; set; } = "Unknown";
+
+        /// <summary>
+        /// Gets or sets the address of the user.
+        /// </summary>
         public string? Address { get; set; }
     }
 }
