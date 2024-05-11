b63f29b32074e58383ab23046af0e2bcbdffb5e3
Danilo Chaku <danilochaku@gmail.com>
2024-05-11 14:55:43 +0300

    docs: document ImageUpload and QuantityExceedsStock exceptions

--- ECommerce.Core/Exceptions/ImageUploadException.cs
+++ ECommerce.Core/Exceptions/ImageUploadException.cs
@@ -1,9 +1,27 @@
 ﻿namespace ECommerce.Core.Exceptions
 {
+    /// <summary>
+    /// Represents an exception that is thrown when an error occurs during image upload.
+    /// </summary>
     public class ImageUploadException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageUploadException"/> class.
+        /// </summary>
         public ImageUploadException() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageUploadException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public ImageUploadException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ImageUploadException"/> class with a specified error message
+        /// and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
         public ImageUploadException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
--- ECommerce.Core/Exceptions/QuantityExceedsStockException.cs
+++ ECommerce.Core/Exceptions/QuantityExceedsStockException.cs
@@ -1,9 +1,27 @@
 ﻿namespace ECommerce.Core.Exceptions
 {
+    /// <summary>
+    /// Represents an exception that is thrown when a quantity exceeds the available stock.
+    /// </summary>
     public class QuantityExceedsStockException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QuantityExceedsStockException"/> class.
+        /// </summary>
         public QuantityExceedsStockException() : base() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QuantityExceedsStockException"/> class with a specified error message.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
         public QuantityExceedsStockException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QuantityExceedsStockException"/> class with a specified error message
+        /// and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception.</param>
         public QuantityExceedsStockException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
