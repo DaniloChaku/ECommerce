d98fc8c6597bdad0e49ce57d50b9760b0fa3abe0
Danilo Chaku <danilochaku@gmail.com>
2024-05-11 12:29:10 +0300

    docs: document the enumerations: PriceType, SortOrder

--- ECommerce.Core/Enums/PriceType.cs
+++ ECommerce.Core/Enums/PriceType.cs
@@ -1,8 +1,18 @@
 ﻿namespace ECommerce.Core.Enums
 {
+    /// <summary>
+    /// Represents the type of pricing for a product.
+    /// </summary>
     public enum PriceType
     {
+        /// <summary>
+        /// Indicates that the price is per item.
+        /// </summary>
         PerItem,
+
+        /// <summary>
+        /// Indicates that the price is per kilogram.
+        /// </summary>
         PerKilo
     }
 }
--- ECommerce.Core/Enums/SortOrder.cs
+++ ECommerce.Core/Enums/SortOrder.cs
@@ -1,8 +1,19 @@
 ﻿namespace ECommerce.Core.Enums
 {
+    /// <summary>
+    /// Represents the sort order for sorting operations.
+    /// </summary>
     public enum SortOrder
     {
+        /// <summary>
+        /// Indicates ascending sort order.
+        /// </summary>
         ASC,
+
+        /// <summary>
+        /// Indicates descending sort order.
+        /// </summary>
         DESC
     }
+
 }
